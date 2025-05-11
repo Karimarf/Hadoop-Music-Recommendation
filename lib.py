80b441029bf57ac2ee3ef1259b251650cdb0b8b8
Karimarf <karimaarfaouim@gmail.com>
2025-05-11 15:41:24 +0200

    edit

--- lib.py
+++ lib.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
         .master("spark://localhost:7077") \
         .config("spark.hadoop.fs.defaultFS", "hdfs://localhost:8020") \
         .getOrCreate()
+    # Read the CSV file from HDFS
 
     df = spark.read.option("header", "true").csv("hdfs://localhost:8020/user/root/donnees/data.csv")
     df.show()
